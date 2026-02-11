99340883458468c0c89b9974955e8f7ebf50c078
Tarik Pinarli <pinarlitarik@.com>
2026-02-11 13:51:46 +0200

    unify configuration and data models for auth and sensors

--- backend/esp32-server/internal/models/modals.go
+++ backend/esp32-server/internal/models/modals.go
@@ -2,14 +2,13 @@ package models
 
 import "time"
 
-// 1. The raw data recieved from esp32
+// --- SENSOR DATA ---
 type IncomingPayload struct {
 	SensorID   string `json:"sensor_id"`
 	Moisture   int    `json:"moisture"`
 	DurationMs int    `json:"duration_ms"`
 }
 
-// 2. the data that will be saved to DB
 type TelemetryData struct {
 	CapturedAt  time.Time `json:"captured_at"`
 	SensorID    string    `json:"sensor_id"`
@@ -17,11 +16,12 @@ type TelemetryData struct {
 	WakeTimeSec float64   `json:"wake_time_seconds"`
 }
 
+// --- USER DATA ---
 type User struct {
 	ID           int       `json:"id"`
 	Login        string    `json:"login"`
-	Email        string    `json:"email"`  
-	Role         string    `json:"role"`       
+	Email        string    `json:"email"`
+	Role         string    `json:"role"`
 	CampusID     int       `json:"campus_id"`
 	SessionToken string    `json:"-"`
 	CreatedAt    time.Time `json:"created_at"`
